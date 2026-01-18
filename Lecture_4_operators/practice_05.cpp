060064e512eb9a20134d10ed5bb32a95820e299d
Er. Deepak <er.mauryad@gmail.com>
2026-01-18 18:40:13 +0530

    Changes under Lecture_4/practice_05.pp

--- Lecture_4_operators/practice_05.cpp
+++ Lecture_4_operators/practice_05.cpp
@@ -1,1 +1,15 @@
-#include
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x;
+    int y;
+
+    cout<<"Enter the value of x and y => "<<endl;
+    cin>>x>>y;
+
+    cout<<((x==5)&&(y==3))<<endl;
+    cout<<((x==5)||(y==3));
+
+    return 0;
+}
